1daa86a74ffec652d1485717359abc16bfac40cc
Paulo Rezende <rezendepauloh@gmail.com>
2022-12-06 23:03:04 -0400

    Estilização da página Start

--- src/Pages/Start/index.jsx
+++ src/Pages/Start/index.jsx
@@ -1,15 +1,16 @@
 import React from "react";
-import { View, Text, Image, ScrollView } from "react-native";
+import { View, Text, Image, ScrollView, StyleSheet } from "react-native";
 
 export default function Start() {
   return (
-    <View>
+    <View style={styles.container}>
       <ScrollView showsVerticalScrollIndicator={false}>
-        <View>
+        <View style={{ alignItems: "center" }}>
           <Image
             source={require("../../assets/icons/logo3.png")}
+            style={styles.logo}
           />
-          <Text>
+          <Text style={styles.description}>
             Vamos transformar sua vida {"\n"} em um jogo, buscando sempre {"\n"} o melhor nível.
           </Text>
         </View>
@@ -17,3 +18,22 @@ export default function Start() {
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "rgba(21, 21, 21, 0.98)",
+  },
+  logo: {
+    width: 300,
+    height: 60,
+    marginTop: 60,
+    marginBottom: 20,
+  },
+  description: {
+    color: "#FFFFFF",
+    fontSize: 20,
+    textAlign: "center",
+    marginVertical: 60,
+  },
+});
